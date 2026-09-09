76ba49bb5a933a4e0750de9820b962554866b5f1
rh-jfuller <jfuller@redhat.com>
2026-09-09 10:51:41 +0200

    security: redact authentication tokens from tracing logs

--- src/bin/stdio.rs
+++ src/bin/stdio.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use common::trustify::Trustify;
+use mcp::logging::RedactingFields;
 use rmcp::{ServiceExt, transport::stdio};
 use tracing_subscriber::{self, EnvFilter};
 
@@ -9,7 +10,8 @@ mod common;
 async fn main() -> Result<()> {
     // Initialize the tracing subscriber with file and stdout logging
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::INFO.into()))
+        .fmt_fields(RedactingFields)
         .with_writer(std::io::stderr)
         .with_ansi(false)
         .init();
--- src/bin/streamhttp.rs
+++ src/bin/streamhttp.rs
@@ -1,4 +1,5 @@
 use mcp::auth;
+use mcp::logging::RedactingFields;
 use rmcp::transport::streamable_http_server::{
     StreamableHttpService, session::local::LocalSessionManager,
 };
@@ -17,9 +18,9 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "debug".to_string().into()),
+                .unwrap_or_else(|_| "info".to_string().into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().fmt_fields(RedactingFields))
         .init();
 
     let service = StreamableHttpService::new(
